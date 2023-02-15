09cc58d528e36f6042cbe1700c549b5f37e65be2
Diya Tes John <diyatesjohn2024a@mca.ajce.in>
2023-02-15 09:48:48 +0530

    first

--- temp_converter.py
+++ temp_converter.py
@@ -1,10 +1,9 @@
-temp=float(input("Enter your temperature:"))
-unit=input("Enter your unit as C or F: ")
-if unit=="c" or unit=="C":
-    f=(temp*1.8)+32
-    print("The temperature in Fahrenheit is: ",f)
-elif unit=="f" or unit=="F":
-    c=(temp-32)/1.8
-    print("The temperature in celsius is: ",c)
-else:
-    print("wrong input...")
+from tkinter import *
+
+master = Tk()
+master.title("Temperature Converter")
+master.geometry('400x200')
+Label(master, text="Temperature in F").grid(row=0, sticky=W)
+Label(master, text="Result:").grid(row=3, sticky=W)
+
+mainloop()
